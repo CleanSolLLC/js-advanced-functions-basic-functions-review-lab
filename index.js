ac5540860c5eb949640ccda91abe783f6551181b
mamark90 <mamark90@gmail.com>
2021-08-10 18:45:25 -0400

    commit tree 8065b006647ce2ee132165e6152f9b68979a32fa

--- index.js
+++ index.js
@@ -1,1 +1,57 @@
-// Your code here
+function saturdayFun(activity="roller-skate") {
+  let fun = `This Saturday, I want to ${activity}!`;
+  return fun;
+}
+
+function mondayWork(activity="go to the office") {
+  let fun = `This Monday, I will ${activity}.`;
+  return fun;
+}
+
+function wrapAdjective(flair="*") {
+  const result = function(description="special") {
+    return `You are ${flair}${description}${flair}!`
+    //const encouragingPromptFunction = wrapAdjective("!!!")
+  }
+       return result;
+}
+
+
+let Calculator = {};
+
+Calculator.add = add;
+Calculator.subtract = subtract;
+Calculator.multiply = multiply;
+Calculator.divide = divide;
+Calculator.actionApplyer = actionApplyer;
+
+function add(num1, num2) {
+  return num1 + num2;
+};
+
+function subtract(num1, num2) {
+  return num1 - num2;
+};
+
+function multiply(num1, num2) {
+  return num1 * num2;
+};
+
+function divide(num1, num2) {
+  return num1 / num2;
+};
+
+function actionApplyer(init, arry) {
+  debugger;
+  let num = arry.reduce(function(accumulator, element){ return element(accumulator)}, init);
+  debugger;
+  return num;
+
+};
+
+
+
+
+
+
+
--- test/indexTest.js
+++ test/indexTest.js
@@ -102,6 +102,7 @@ describe("index.js", () => {
       })
 
       it("Given 13, returns 4 after being acted on by several functions", function() {
+        debugger;
         let message = "13, multiplied by 2, added to 1000 and then modulo 7 is 4. Apply each function in the Array of functions on the given base (13) OR on the result of the use of the previous function to get this result"
         arrayOfTransforms = [
           function(a){ return a * 2 },
